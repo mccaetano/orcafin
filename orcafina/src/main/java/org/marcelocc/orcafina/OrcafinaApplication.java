352705a8bff8f8cbb63271cd770f6aad1c1dd808
marcelocc@hdi.br <marcelo.caetano@hdi.com.br>
2024-09-18 08:17:30 -0300

    initial

---
+++ orcafina/src/main/java/org/marcelocc/orcafina/OrcafinaApplication.java
@@ -0,0 +1,13 @@
+package org.marcelocc.orcafina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrcafinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrcafinApplication.class, args);
+	}
+
+}
